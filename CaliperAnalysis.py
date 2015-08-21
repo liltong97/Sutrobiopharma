2eeb9acd08ee5b29f9dc83d0d779b948b3441eee
liltong97 <lillianjietong@gmail.com>
2015-08-21 11:36:53 -0700

    Successfully implemented pulling std peaks function

--- CaliperAnalysis.py
+++ CaliperAnalysis.py
@@ -6,16 +6,45 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 
-def pullstdcurvedata(stdcurverow, stdcurveplate):
+def pullstdcurvedata(stdcurverow, stdcurveplate, peakname):
     ''' Finds the herceptin curve data and returns it. Assumes that there
     is a duplicate on the next row and it takes up 12 wells/row'''
-    rowApos = df['Sample Name'] == 'A1'
-    startrowApos = df[rowApos]['Type']=='?'
     
+    startwell = stdcurverow.upper() + '1'
+    # Find start of herceptin curve
+    rowpos = df['Sample Name'] == startwell
+    startrowpos = df[rowpos]['Type']=='?' # every well starts with a ? peak
+    startingstdindex= df[rowpos][startrowpos].index.tolist()
+    ilocstartindex= startingstdindex[stdcurveplate-1]
+    df_afterstart = df.iloc[ilocstartindex:]
+    
+    # Find end of herceptin curve
+    lastwellofstd = df_afterstart['Sample Name'] ==chr(ord(stdcurverow.upper())+1) +'12'
+    lastwellindices =  df_afterstart[lastwellofstd].index.tolist()
+    ilocendindex = lastwellindices[-1]
+    herceptinstdwells = df.iloc[ilocstartindex:ilocendindex]  
+    
+    # pull out desired peaks 
+    identifiedpeaks = herceptinstdwells['Type'] == peakname
+    forcedpeaks = herceptinstdwells['Type'] == peakname + '*'
+    allpeaks = identifiedpeaks | forcedpeaks
+    df_stdcurve = herceptinstdwells[allpeaks] 
+    
+    return df_stdcurve
 
+def stdcurvequadfit(df_stdcurve, dispgraph):
+    ''' Fits a quadratic curve to the herceptin standard data''' 
+    fitdata = df_stdcurve['Corr. Area']
+    x= [10, 50, 100, 200, 300, 400, 500, 600, 700, 800, 900, 1000] #doublecheck
+    # do polyfit...
     
+    if dispgraph:
+        plot(x, fitdata)
+        
+        # plot fitted data ontop...
+        
+def calculateconc(df, curvefitparam):
     
-
     
 
 
@@ -24,8 +53,8 @@ df = pd.read_csv(fname, comment='#')
 
 stdcurverow = 'A'
 stdcurveplate = 2
-
-# stddata = pullsstdcurvedata(stdcurverow, stdcurveplate)
+stdpeakname = 'IgG'
+stddata = pullstdcurvedata(stdcurverow, stdcurveplate, stdpeakname)
 
     
     
