578e4b2b2ad535634f388294db5bb8967e93dd72
liltong97 <lillianjietong@gmail.com>
2015-10-26 17:01:23 -0700

    Major bug fixes with program

--- CaliperAnalysis.py
+++ CaliperAnalysis.py
@@ -7,6 +7,7 @@ import pandas as pd
 import numpy as np
 import scipy.stats as stats
 import xlsxwriter
+import Tkinter
 
 def pullstdcurvedata(df, stdcurverow, stdcurveplate, peakname):
     ''' Finds the herceptin curve data and returns it. Assumes that there
@@ -45,23 +46,23 @@ def findmissingwells(df, wholeplate):
     startingwell = wells.iloc[0]
     startingrow = startingwell[0]
     startingcolumn = startingwell[1:3]
+    print(startingcolumn)
     if startingcolumn != '1':
-        for i in np.linspace(1, int(startingcolumn), int(startingcolumn)+1):
-            missingwells += [startingrow + str(i)]
+        for i in np.linspace(1, int(startingcolumn)-1, int(startingcolumn)-1):
+            missingwells += [startingrow + str(int(i))]
         
     if wholeplate:
         lastwell = 'H12'
     else:
         lastwell = wells.iloc[-1]
     theoreticalnumwells = 12* (ord(lastwell[0]) - ord(startingrow)+1)
-    
     for j in np.linspace(0, theoreticalnumwells-1, theoreticalnumwells):
-        currwellnum = (j % 12)+1
+        currwellnum = (int(j) % 12)+1
         currwellrow = chr(int(ord(startingrow) + j/12))
         currwell = currwellrow + str(int(currwellnum))
         if not currwell in wells.values:
            missingwells += [currwell] 
-    return missingwells
+    return list(set(missingwells))
       
       
 def calcavgstd(df_stdcurve):
@@ -104,8 +105,6 @@ def stdcurvequadfit(df_stdcurve, dispgraph):
     x = np.array([1000, 800, 600, 400, 200, 150, 100, 80, 60 ,40, 20, 10])
     avgstdcurve = calcavgstd(df_stdcurve)
     avgstdcurvetrim = avgstdcurve
-    #indices = [i for i, x in enumerate(avgstdcurve) if np.isnan(x)]
-    #test = [j for k, j in enumerate(x) if k not in indices]
     counter =0
     for j in np.linspace(0, len(avgstdcurve)-1, len(avgstdcurve)):
         if np.isnan(avgstdcurve[j]):
@@ -127,10 +126,12 @@ def stdcurvequadfit(df_stdcurve, dispgraph):
         
     return fitparams
     
-def pulldata(notstddata, peakname):
+def pulldata(notstddata, scaff):
     # pull out desired peaks 
-    identifiedpeaks = notstddata['Type'] == peakname
-    forcedpeaks = notstddata['Type'] == peakname + '*'
+    scaffolds = scaff['Scaffold'].unique()
+    forcedscaffolds = [scaff + "*" for scaff in scaffolds]
+    identifiedpeaks = notstddata['Type'].isin(scaffolds)
+    forcedpeaks = notstddata['Type'].isin(forcedscaffolds)
     allpeaks = identifiedpeaks | forcedpeaks
     sampledata = notstddata[allpeaks]
     
@@ -139,33 +140,36 @@ def pulldata(notstddata, peakname):
 def calculateconc(sampledata, curvefitparam, df_scaff):
     sampcorrarea = sampledata['Corr. Area']
     concentrations = curvefitparam[0]*sampcorrarea**2 + curvefitparam[1] *sampcorrarea + curvefitparam[2]
-    conversions = {'IgG': 150000, 'GFP': 28000, 'scfvfc': 100000, 'scfv': 23000, 'IgG*':150000}
-    combineddf = pd.merge(sampledata, df_scaff, on=['Well Label'])
-    combineddf = combineddf.replace({"Scaffold":conversions})
+    conversions = {'IgG': 150000, 'GFP': 28000, 'scFvFc': 100000, 'scfv': 23000}
+    combineddf_full = pd.merge(sampledata, df_scaff, on=['Well Label'])
+    #combineddf_full = combineddf_full.replace({"Scaffold":conversions})
     conc =  []
     nMconc = []
-    for i in np.linspace(0, len(combineddf)-1, len(combineddf)):
-        nMconc += [(concentrations.iloc[i] / combineddf['Scaffold'].iloc[i].astype(float)) * 10**6]
+    for i in np.linspace(0, len(combineddf_full)-1, len(combineddf_full)):
+        scaffnorm = combineddf_full['Scaffold'].iloc[i]
+        nMconc += [(concentrations.iloc[i] / conversions[scaffnorm]) * 10**6]
         conc += [concentrations.iloc[i]]
-    combineddf['[IgG], ug/mL'] = conc
-    combineddf['nM'] = nMconc
+    combineddf_full['[IgG], ug/mL'] = conc
+    combineddf_full['nM'] = nMconc
 
-    return combineddf
+    return combineddf_full
 
-    
-fname_raw = '..\ltong\Documents\GitHub\Sutrobiopharma\CaliperPeakTable.csv'
+top = Tkinter.Tk()
+top.mainloop()
+
+fname_raw = '..\ltong\Documents\Caliper Data\Raw Data\pHexpsamples_2015-10-22_03-27-53_PeakTable.csv'
 df_raw = pd.read_csv(fname_raw, comment='#')
-fname_scaff = '..\ltong\Documents\GitHub\Sutrobiopharma\scaffold.csv'
+fname_scaff = '..\ltong\Documents\Caliper Data\Raw Data\scaffold_1988.csv'
 df_scaff = pd.read_csv(fname_scaff, comment='#')
 
-stdcurverow = 'A'
+stdcurverow = 'G'
 stdcurveplate = 2
 stdpeakname = 'IgG'
 stddata, notstddata = pullstdcurvedata(df_raw, stdcurverow, stdcurveplate, stdpeakname)
 equationparams = stdcurvequadfit(stddata, True)
-sampledata = pulldata(notstddata, stdpeakname)
+sampledata = pulldata(notstddata, df_scaff)
 fulldf = calculateconc(sampledata, equationparams,df_scaff)
-trimmeddf = pd.concat([fulldf['Well Label'], fulldf['Sample Name'], fulldf['Variant ID'], fulldf['Type'], fulldf['[IgG], ug/mL'], fulldf['nM']], axis=1)
+trimmeddf = pd.concat([fulldf['Well Label'], fulldf['Sample Name'], fulldf['Variant ID'], fulldf['Scaffold'], fulldf['Type'].str.rstrip('*'), fulldf['[IgG], ug/mL'], fulldf['nM']], axis=1)
 
 addwells = findmissingwells(trimmeddf, True)
 
@@ -178,13 +182,28 @@ for i in range(len(addwells)):
     a=df_scaff[df_scaff['Well Label'] == welllabel]
     varID = a['Variant ID'].iloc[0]
     scaff = a['Scaffold'].iloc[0]
-    trimmeddf.loc[i+len(trimmeddf)] = [welllabel, sample, varID, scaff, 'Not Detected', 0]
-    
-exportdf = trimmeddf.sort(['Well Label'])
+    trimmeddf.loc[i+len(trimmeddf)] = [welllabel, sample, varID, scaff, scaff, 'Not Detected', 0]
+desiredscaffind = trimmeddf['Scaffold'] == trimmeddf['Type']
+exportdf_trim = trimmeddf[desiredscaffind]
+addwells2 = findmissingwells(exportdf_trim, True)
+for i in range(len(addwells2)):
+    sample = addwells2[i]
+    if len(sample) == 2:
+        welllabel= sample[0] + '0' + sample[1]
+    else:
+        welllabel = sample
+    a=df_scaff[df_scaff['Well Label'] == welllabel]
+    varID = a['Variant ID'].iloc[0]
+    scaff = a['Scaffold'].iloc[0]
+    exportdf_trim.loc[i+len(exportdf_trim)] = [welllabel, sample, varID, scaff, scaff, 'Not Detected', 0]
+exportdf = exportdf_trim.sort(['Well Label'])
+del exportdf['Type']
+exportdf_full = trimmeddf.sort(['Well Label'])
     
 # Getting dumped: C:\Users\ltong
 writer = pd.ExcelWriter('simple.xlsx', engine='xlsxwriter')
 exportdf.to_excel(writer, sheet_name='Sheet1')
+exportdf_full.to_excel(writer, sheet_name='Sheet2')
 writer.save()
 
 
